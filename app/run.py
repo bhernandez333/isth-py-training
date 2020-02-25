1bfd69cbbd0cbe19725bd0ef00cac8104b91a6ba
bhernandez333 <61214858+bhernandez333@users.noreply.github.com>
2020-02-24 22:24:01 -0600

    Update run.py

--- app/run.py
+++ app/run.py
@@ -2,3 +2,8 @@ from app.adaptor import file_routes as file
 from app.domain.model import Usuario
 
 print('Iniciando')
+U = Usuario()
+U.Username = "Usuario"
+U.Nombre = "Nombre"
+U.Apellido = "Apellido"
+file.create_file("nombre.txt", U)
